0ef485f41efab036aa8fa3b95822f59a6d0f60b1
Quim <quimifrances@gmail.com>
2021-02-28 16:36:13 +0100

    PropTypes, Props, DefaultProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,21 +1,27 @@
 import { Fragment } from "react";
+import PropTypes from 'prop-types';
 
 
-
-const PrimeraApp = ( {saludo = "Hola, soc en  Quim Francès."}) => {
-
-    
+const PrimeraApp = ( { saludo, subtitulo }) => {
 
     return (
         <Fragment>
             <h1> { saludo } </h1>
-            <p>Mi primera app</p>
+            <p> { subtitulo } </p>
         </Fragment>
     )
 
 }
-    
 
+PrimeraApp.propTypes = {
 
+    saludo: PropTypes.string.isRequired,
+
+}
+
+PrimeraApp.defaultProps = {
+    saludo: 'Hola World, is getting dark downhere...',
+    subtitulo: 'Hola soy un subtitulo pequeñin.',
+}
 
 export default PrimeraApp;
