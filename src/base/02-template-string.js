a9ab48cbbcf39d54b351d720d027321efb77c8be
Quim <quimifrances@gmail.com>
2021-03-04 14:04:33 +0100

    Templatte String Test Passing

--- src/base/02-template-string.js
+++ src/base/02-template-string.js
@@ -8,11 +8,15 @@ const apellido = 'Herrera';
 // const nombreCompleto = nombre + ' ' + apellido;
 const nombreCompleto = `${ nombre } ${ apellido }`;
 
-console.log( nombreCompleto );
+//console.log( nombreCompleto );
 
 
-function getSaludo(nombre) {
-    return 'Hola ' + nombre;
+export function getSaludo(nombre) {
+    if (nombre) {
+        return 'Hola ' + nombre;
+    }else{
+        return 'Hola Carlos!';
+    }
 }
 
-console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
+//console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
---
+++ src/tests/base/02-template-string.test.js
@@ -0,0 +1,22 @@
+import { getSaludo } from '../../base/02-template-string';
+
+describe('pruebas en template-string.js', () => {
+
+    
+    test('getSaludo retorna Saludo', () => {
+            const nombre= 'Quim';
+
+            const saludo = getSaludo(nombre);
+
+            expect(saludo).toBe('Hola ' + nombre);
+    })
+
+    test('debe retornar "Hola CArlos!" si no hay parametro nombre', () => {
+            
+            const saludo = getSaludo();
+
+            expect(saludo).toBe('Hola Carlos!');
+    })
+    
+    
+})
